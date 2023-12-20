26d9e5987aa0e5dde374bf2e66cf194965e6c7d7
BilalKhaalid <126148067+BilalKhaalid@users.noreply.github.com>
2023-12-20 15:38:03 +0500

    exporting a module in node js

--- Section-1/module.js
+++ Section-1/module.js
@@ -1,6 +1,7 @@
+// ! This is a module
 const add = (number, number2) => {
   return number + number2;
 };
 
-const sum = add(15, 15);
-console.log(sum);
+// ! to export a module in node we rely on commonJs
+module.exports = add;
