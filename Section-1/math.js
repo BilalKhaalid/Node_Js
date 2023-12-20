fdeaa940ee1a86c25f49a4cf2619d9ee89f4cc48
BilalKhaalid <126148067+BilalKhaalid@users.noreply.github.com>
2023-12-20 20:43:46 +0500

    Third Pattern When we want to export more than one modules

--- Section-1/math.js
+++ Section-1/math.js
@@ -3,10 +3,20 @@ const add = (a, b) => {
   return a + b;
 };
 
+const subtract = (a, b) => {
+  return a - b;
+};
+
 // ? This is the common module export
 // module.exports = add;
 
 // ? Second Pattern
-module.exports = (a, b) => {
-  return a + b;
+// module.exports = (a, b) => {
+//   return a + b;
+// };
+
+// ? Third Pattern When we want to export more than one modules
+module.exports = {
+  add,
+  subtract,
 };
