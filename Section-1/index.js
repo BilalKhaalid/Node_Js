ebe771491bdfe0d74bd3821ae0fbdbe1bdf2947a
BilalKhaalid <126148067+BilalKhaalid@users.noreply.github.com>
2023-12-20 20:17:51 +0500

    explained with examples on how memory caching works in node and how to prevent it

--- Section-1/index.js
+++ Section-1/index.js
@@ -1,10 +1,36 @@
-console.log("Hello World!");
+// console.log("Hello World!");
 
 // ! To import or use a module in another file we use require function
-const add = require("./module");
+// const add = require("./module");
 
-const sum = add(15, 15);
-console.log(sum);
+// const sum = add(15, 15);
+// console.log(sum);
 
-require("./batman");
-require("./superman");
+// require("./batman");
+// require("./superman");
+
+// ! memory caching concept importing the exported instance of class SuperHero
+// const SuperHero = require("./super-hero");
+// console.log(SuperHero);
+// console.log(SuperHero.getName());
+// console.log(SuperHero.setName("Bilal Khalid"));
+// console.log(SuperHero);
+
+// const SuperHero2 = require("./super-hero");
+// // * SuperHero2 will get the same name as we have set it in the previous code this is because node uses memory caching to increase performance and it will not reload the module
+// console.log(SuperHero2.getName());
+
+// ! memory caching concept importing the class SuperHero itself
+const SuperHero = require("./super-hero");
+console.log(SuperHero);
+
+const Bilal = new SuperHero("Bilal");
+console.log(Bilal);
+
+const Bilal_Khalid = Bilal.setName("Bilal Khalid");
+console.log(Bilal_Khalid);
+
+const newSuperHero = require("./super-hero");
+const SuperHero2 = new newSuperHero("John");
+
+console.log(SuperHero2.getName());
