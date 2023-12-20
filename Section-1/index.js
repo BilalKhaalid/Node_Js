b643db843b047a4558c2345c516b4852d41ed7a4
BilalKhaalid <126148067+BilalKhaalid@users.noreply.github.com>
2023-12-20 15:39:59 +0500

    require is used to import a module in another file

--- Section-1/index.js
+++ Section-1/index.js
@@ -1,1 +1,7 @@
 console.log("Hello World!");
+
+// ! To import or use a module in another file we use require function
+const add = require("./module");
+
+const sum = add(15, 15);
+console.log(sum);
