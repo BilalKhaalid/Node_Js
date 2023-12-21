70e1486c4e7fdf4a63194e7b3e7e78534ccd39c5
BilalKhaalid <126148067+BilalKhaalid@users.noreply.github.com>
2023-12-21 15:34:58 +0500

    First export default pattern

---
+++ Section-1/ESModules.mjs
@@ -0,0 +1,9 @@
+// ! Exporting modules using ES Modules
+// * ES Modules came in ES2015(ES6) and in node v14 it came official in node
+
+const add = (a, b) => {
+  return a + b;
+};
+
+// ! First Pattern export default
+export default add;
