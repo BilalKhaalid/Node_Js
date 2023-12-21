973f2a45e78b10cfccd1ec6af8e9e713b42d9812
BilalKhaalid <126148067+BilalKhaalid@users.noreply.github.com>
2023-12-21 15:45:40 +0500

    When we want to make a named export. Note that when importing it should be importing using same name as exported

--- Section-1/ESModules.mjs
+++ Section-1/ESModules.mjs
@@ -1,24 +1,29 @@
 // ! Exporting modules using ES Modules
 // * ES Modules came in ES2015(ES6) and in node v14 it came official in node
 
-const add = (a, b) => {
-  return a + b;
-};
+// const add = (a, b) => {
+//   return a + b;
+// };
 
-const subtract = (a, b) => {
-  return a - b;
-};
+// const subtract = (a, b) => {
+//   return a - b;
+// };
 
-function multiply(a, b) {
-  return a * b;
-}
+// function multiply(a, b) {
+//   return a * b;
+// }
 
 // ! First Pattern export default
 // export default add;
 
 // ! When we want to export more than one module we use second pattern
-export default {
-  add,
-  subtract,
-  multiply,
-};
+// export default {
+//   add,
+//   subtract,
+//   multiply,
+// };
+
+// ! When we want to make a named export. Note that when importing it should be importing using same name as exported
+export function multiply(a, b) {
+  return a * b;
+}
