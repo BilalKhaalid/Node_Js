3f32d8a76fb1a398229e349471bc97c25f209915
BilalKhaalid <126148067+BilalKhaalid@users.noreply.github.com>
2023-12-22 12:31:05 +0500

    Setting routes in http module

--- BIM/http.js
+++ BIM/http.js
@@ -14,17 +14,34 @@ const server = http.createServer((req, res) => {
   //   res.end(JSON.stringify(obj));
 
   // ! Sending HTML as response
-  res.writeHead(200, { "Content-Type": "text/html" });
+  //   res.writeHead(200, { "Content-Type": "text/html" });
   //   const html = fs.readFileSync(__dirname + "/index.html", "utf-8");
   //   res.end(html);
   // ! Or we can use pipe to do the job
   //   fs.createReadStream("./index.html").pipe(res);
 
   // ! changing html template in response
-  let name = "Bilal";
-  let html = fs.readFileSync(__dirname + "/index.html", "utf-8");
-  html = html.replace("{{name}}", name);
-  res.end(html);
+  //   let name = "Bilal";
+  //   let html = fs.readFileSync(__dirname + "/index.html", "utf-8");
+  //   html = html.replace("{{name}}", name);
+  //   res.end(html);
+
+  // ! Setting routes in http module
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  if (req.url === "/") {
+    res.end("Home Page");
+  } else if (req.url === "/about") {
+    res.end("About Page");
+  } else if (req.url === "/api") {
+    res.end(
+      JSON.stringify({
+        firstName: "Bilal",
+        lastName: "Khalid",
+      })
+    );
+  } else {
+    res.end("Page not found");
+  }
 });
 const port = 5000;
 server.listen(port, () => {
