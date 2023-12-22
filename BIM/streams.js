859178e43066a112f238b9b5b047ab109b28f334
BilalKhaalid <126148067+BilalKhaalid@users.noreply.github.com>
2023-12-22 11:11:44 +0500

    Analogy : Streaming Music

--- BIM/streams.js
+++ BIM/streams.js
@@ -1,4 +1,11 @@
 // ! Streams In Node JS
+// ! Analogy: Streaming Music
+
+//     Simple Explanation:
+//         Imagine you're listening to music on your favorite streaming service. The song doesn't load all at once; it plays as the data (the song) streams to your device. You hear the beginning while the rest is still loading.
+
+//     Relating to Node.js:
+//         In Node.js, a "stream" is like this continuous flow of data. Instead of waiting for the entire data to load, it's processed in chunks, allowing you to start using the data as soon as it's available.
 const fs = require("node:fs");
 
 const readableStream = fs.createReadStream("./file.txt", {
@@ -12,9 +19,14 @@ const writeableStream = fs.createWriteStream("./file2.txt", {
 });
 
 readableStream.on("data", (chunk) => {
-  console.log(chunk);
+  console.log(`Received a chunk of data: ${chunk}`);
   writeableStream.write(chunk);
 });
 
-const file = fs.readFileSync("./file2.txt", "utf-8");
-console.log(file);
+//! Event handler for when the stream ends
+readableStream.on("end", () => {
+  console.log("Finished reading the file.");
+});
+
+// const file = fs.readFileSync("./file2.txt", "utf-8");
+// console.log(file);
