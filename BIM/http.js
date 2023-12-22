c678831b30629403964f8bafe19117610652ccb1
BilalKhaalid <126148067+BilalKhaalid@users.noreply.github.com>
2023-12-22 12:02:52 +0500

    Sending HTML as response

--- BIM/http.js
+++ BIM/http.js
@@ -1,16 +1,22 @@
 // ! Built In http module
 const http = require("node:http");
+const fs = require("node:fs");
 
 const server = http.createServer((req, res) => {
   //   res.writeHead(200, { "Content-Type": "text/plain" });
   //   res.end("Hello World!");
-  // ! Sending JSON as response
-  const obj = {
-    firstName: "Bilal",
-    lastName: "Khalid",
-  };
-  res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(JSON.stringify(obj));
+  //   // ! Sending JSON as response
+  //   const obj = {
+  //     firstName: "Bilal",
+  //     lastName: "Khalid",
+  //   };
+  //   res.writeHead(200, { "Content-Type": "application/json" });
+  //   res.end(JSON.stringify(obj));
+
+  // ! Sending HTML as response
+  res.writeHead(200, { "Content-Type": "text/html" });
+  const html = fs.readFileSync(__dirname + "/index.html", "utf-8");
+  res.end(html);
 });
 const port = 5000;
 server.listen(port, () => {
