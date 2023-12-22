1c829ec3eeb4f5a5e489a89b1db0d14199d79751
BilalKhaalid <126148067+BilalKhaalid@users.noreply.github.com>
2023-12-22 11:52:27 +0500

    Sending JSON as response

--- BIM/http.js
+++ BIM/http.js
@@ -2,8 +2,15 @@
 const http = require("node:http");
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200);
-  res.end("Hello World!");
+  //   res.writeHead(200, { "Content-Type": "text/plain" });
+  //   res.end("Hello World!");
+  // ! Sending JSON as response
+  const obj = {
+    firstName: "Bilal",
+    lastName: "Khalid",
+  };
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(JSON.stringify(obj));
 });
 const port = 5000;
 server.listen(port, () => {
